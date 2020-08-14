a9531f73b89c4b6c7a7daa8515f32217de080862
j-elliot <jem060@bucknell.edu>
2020-08-14 09:41:35 -0400

    I forgot to push this and now what a pain

--- DiscordChatbotA.1/bot.js
+++ DiscordChatbotA.1/bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('../../offline/auth.json');
+var fs = require('fs');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 
@@ -23,6 +24,42 @@ do { d2 = new Date(); }
 while(d2-d < ms);
 }
 
+function respond(dm)
+{
+
+  if(dm.userID != bot.id) {
+
+    fs.writeFileSync('../textCom/input.json', JSON.stringify(dm));
+
+    for(i = 500; i < 3000; i += 500){
+
+      try {
+        var rawdata = fs.readFileSync('../textCom/output.json');
+        var response = JSON.parse(rawdata);
+      }
+      catch(err) {
+        logger.info('Hesitating...');
+        var response = {
+          time: new Date()
+        }
+      }
+
+      if(JSON.parse(JSON.stringify(dm)).time == response.time){
+
+        return response.message;
+
+      }
+
+      wait(i);
+
+    }
+
+    return "I lost my train of thought...";
+
+  }
+
+}
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -31,59 +68,26 @@ bot.on('ready', function (evt) {
 
 bot.on('message', function (user, userID, channelID, message, evt){
     logger.info('Running message subroutine with ' + message);
+    var discordMessage = {
+      user: user,
+      userID: userID,
+      channelID: channelID,
+      message: message,
+      evt: evt,
+      time: new Date()
+    }
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     if (true)
     {
-        logger.info('Responding... ' + message);
-        var args = message.split(' ');
-        var cmd = args[0];
-        wait(2000);
-        args = args.splice(1);
-        if(message != "Kyle? I heard that guy's a bitch!")
-        {
-          if(message.toLowerCase().search("kyle") > -1)
-          {
-            bot.sendMessage({
-                to: channelID,
-                message: "Kyle? I heard that guy's a bitch!"
-            });
-          }
-        }
-        if(message != "Ardjen? You've got to be kidding me! Don't talk to me about Ardjen.")
-        {
-          if(message.toLowerCase().search("ardjen") > -1)
-          {
-            bot.sendMessage({
-                to: channelID,
-                message: "Ardjen? You've got to be kidding me! Don't talk to me about Ardjen."
-            });
-          }
-        }
-        switch(message.toLowerCase())
-        {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: message.substring(1).split(' ')
-                });
-            break;
-            case 'hello':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hi!'
-                });
-            break;
-            case 'how are you?':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'same as always.'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
+      logger.info('Responding... ' + message);
+      wait(2000);
 
-     }
+      bot.sendMessage({
+        to: channelID,
+        message: respond(discordMessage)
+      });
+
+
+    }
 
 });
